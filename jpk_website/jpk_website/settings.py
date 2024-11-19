40dd4d528429c6907bfc3bba57c21e7b2c33c6a2
Jeremy Kuehn <100542455+arkterra90@users.noreply.github.com>
2024-11-19 09:55:14 -0600

    Update settings.py

--- jpk_website/jpk_website/settings.py
+++ jpk_website/jpk_website/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+#DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 
 #Digital Ocean Var for knowing when to connect to postGre DB
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
