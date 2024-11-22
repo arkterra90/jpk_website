8da96238affd00d5cd940f3105996d6f7db36c3f
Jeremy Kuehn <100542455+arkterra90@users.noreply.github.com>
2024-11-21 21:01:28 -0600

    removes static root

--- jpk_website/jpk_website/settings.py
+++ jpk_website/jpk_website/settings.py
@@ -130,7 +130,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # DigitalOcean Spaces settings
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')  # Your access key
